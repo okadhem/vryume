a5adbc939eae3f50cee6d1cff4e609cdde8e6290
kadhem <okadhem@google.com>
2026-06-14 23:19:56 +0200

    wip materials

--- src/common.h.glsl
+++ src/common.h.glsl
@@ -1,13 +1,13 @@
 // TODO: Keep in sync with the one in cpp until we use a global define or some solution
-const float inter_sample_distance = 0.002;        // in m
+const float inter_sample_distance = 0.002; // in m
 
-const float grad_epsilon = 1 * inter_sample_distance; 
+const float grad_epsilon = 1 * inter_sample_distance;
 
 // given in terms of sdf values, the maximum depth we will reach while computing the gradiant on the surface
 const float max_surface_penetration = -sqrt(3) * grad_epsilon; // sqrt(3) is the norm of the k vectors used in the grad
 
 // values are read from the tile by interpolating 8 surrounding sdf values, all these values
-// should be representable. therefore we need to represent the sdf of the point along the 
+// should be representable. therefore we need to represent the sdf of the point along the
 // diagonal of the max_surface_penetration, that is the "-sqrt(3) * inter_sample_distance" term.
 const float min_representable_distance = max_surface_penetration + (-sqrt(3) * inter_sample_distance);
 const float encoded_distance_resolution = 0.0002; // in m
@@ -22,13 +22,15 @@ float inverse_mix(float a, float b, float x)
 }
 
 float encode_distance(float distance) {
-    float clamped = clamp(distance, min_representable_distance, max_representable_distance); 
+    float clamped = clamp(distance, min_representable_distance, max_representable_distance);
     return inverse_mix(min_representable_distance, max_representable_distance, clamped);
 }
 
 float decode_distance(float sampled_distance) {
-  return mix(min_representable_distance, max_representable_distance,
-             sampled_distance);
+    return mix(min_representable_distance, max_representable_distance,
+        sampled_distance);
 }
 
 const float MIN_FLOAT = -3.402823e38;
+
+const uint UINT_MAX = 0xFFFFFFFFu;
